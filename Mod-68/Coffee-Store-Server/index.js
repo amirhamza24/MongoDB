658ba99b4b1855ff7d430854ebae565048368e19
Amir Hamza <52718336+amirhamza24@users.noreply.github.com>
2023-05-26 23:19:21 +0600

    Update index.js

--- Mod-68/Coffee-Store-Server/index.js
+++ Mod-68/Coffee-Store-Server/index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
+// middleware 
 app.use(cors());
 app.use(express.json());
 
